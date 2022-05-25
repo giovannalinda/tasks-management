8c9b04f5da0244db605ae0e95e683fd5699aa2e1
Giovanna Linda <eugiovannasouza@gmail.com>
2022-05-25 14:59:10 -0300

    feat: adding project theme

---
+++ src/config/theme.ts
@@ -0,0 +1,20 @@
+export const theme = {
+  colors: {
+    white: '#FFFFFF',
+    gray: {
+      700: '#191924',
+      900: '#070710',
+    },
+    purple: {
+      600: '#7033C8',
+    },
+  },
+  radius: '5px',
+  font: {
+    family: 'Dm Sans',
+    weigths: {
+      regular: 400,
+      bold: 700,
+    },
+  },
+}
